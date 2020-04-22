4648352188d4c277327414ccd51adc5cfdab5687
lindacoco <lindacoco0204@gmail.com>
2020-04-22 14:59:54 +0900

    sql

---
+++ Scripts/Script.sql
@@ -0,0 +1,16 @@
+use jhs_project;
+create table project (
+  no int auto_increment,
+  name varchar(20) null,
+  content varchar(200) null,
+  regDate timestamp not null default now(),
+  endDate timestamp null,
+  progress varchar(10) null,
+  primary key(no)
+);
+
+desc project ;
+drop table project;
+select * from project;
+
+insert into project(name,content,regDate,endDate,progress)(select name, content, regDate, endDate,progress from project);
